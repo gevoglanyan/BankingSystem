2a2a8cee2635e1af437a89ce830dbb76d92d6959
Maxwell Kozlov <maxwellkozlov1@gmail.com>
2024-11-27 18:48:48 -0800

    Wrote transactionsRelatedToAccount, transactionBetweenDates, and getLoanFromCustomer

--- DatabaseConnectionTester.java
+++ DatabaseConnectionTester.java
@@ -1,8 +1,10 @@
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class DatabaseConnectionTester {
 
@@ -11,14 +13,32 @@ public class DatabaseConnectionTester {
             Connection connection = DatabaseConnection.getConnection();
             System.out.println("Connected to database");
 
-            ResultSet rs = db_query.customerQuery1("professor1@gmail.com");
-            db_query.printRs(rs);
+//            ResultSet rs = db_query.customerQuery1("professor1@gmail.com")
+//            db_query.printRs(rs);
+//
+//            ResultSet rs2 = db_query.getCustomerByAddress("hogwarts");
+//            db_query.printRs(rs2);
 
-            ResultSet rs2 = db_query.getCustomerByAddress("hogwarts");
-            db_query.printRs(rs2);
+//            int rs3 = db_query.createCustomerQuery("Maxwell","Kozlov","CSUN","maxwell.kozlov.595@my.csun.edu","818-523-6396");
+//            Boolean status = db_query.checkIfEmailExists("maxwell.kozlov.55@my.csun.edu");ed
+//            System.out.println(status);
+
+//            ResultSet rs4 = db_query.transactionsRelatedToAccount(1);
+//            db_query.printRs(rs4);
+
+//                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//                Date fromDate = formatter.parse("2024-11-12 00:00:00");
+//                Date toDate = formatter.parse("2024-11-30 23:59:59");
+//                ResultSet rs5 = db_query.transactionsBetweenDates(Timestamp.from(fromDate.toInstant()), Timestamp.from(toDate.toInstant()));
+//                db_query.printRs(rs5);
+
+
+            ResultSet rs6 = db_query.getLoansFromCustomer(1);
+            db_query.printRs(rs6);
 
 
         } catch (Exception e) {
+            e.printStackTrace();
             System.out.println("Error connecting to database: " + e.getMessage());
         }
     }
--- db_query.java
+++ db_query.java
@@ -1,3 +1,4 @@
+import javax.xml.transform.Result;
 import java.sql.*;
 
 public class db_query {
@@ -18,10 +19,17 @@ public class db_query {
         return conn;
     }
 
+
+    //
+    // DEFINED BUISNESS LOGIC
+    //
+
+
     public static ResultSet customerQuery1(String email) throws SQLException {
         ResultSet rs = null;
         String sql = "SELECT * FROM customer WHERE email = ?";
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, email); 
             rs = pstmt.executeQuery();
             System.out.println("Query Successful");
@@ -30,6 +38,110 @@ public class db_query {
         }
         return rs;
     }
+
+    public static ResultSet transactionsRelatedToAccount(int accountNumber) throws SQLException{
+        ResultSet rs = null;
+        String sql = "SELECT * FROM transaction WHERE senderNum = ? or recieverNum = ?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, accountNumber);
+            pstmt.setInt(2, accountNumber);
+            rs = pstmt.executeQuery();
+            System.out.println("Query Successful");
+        } catch (SQLException e) {
+            System.err.println("Error with Transaction Related To Query: " + e.getMessage());
+            System.err.println(e.getErrorCode());
+
+        }
+        return rs;
+    }
+
+    public static ResultSet transactionsBetweenDates(Timestamp from, Timestamp to) throws SQLException{
+        ResultSet rs = null;
+        String sql = "SELECT * FROM transaction WHERE transactionDate >= ? and transactionDate <= ?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setTimestamp(1, from);
+            pstmt.setTimestamp(2, to);
+            rs = pstmt.executeQuery();
+            System.out.println("Query Successful");
+        } catch (SQLException e) {
+            System.err.println("Error with Transaction Between Dates Query: " + e.getMessage());
+            System.err.println(e.getErrorCode());
+        }
+        return rs;
+    }
+
+
+    public static ResultSet getLoansFromCustomer(int customerID) throws SQLException{
+        ResultSet rs = null;
+        String sql = "SELECT * FROM loan WHERE customerID = ?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, customerID);
+            rs = pstmt.executeQuery();
+            System.out.println("Query Successful");
+        } catch (SQLException e) {
+            System.err.println("Error with Get Loans From Customer Query: " + e.getMessage());
+            System.err.println(e.getErrorCode());
+        }
+        return rs;
+    }
+
+
+    //
+    // UTILITY FUNCTIONS
+    //
+
+
+    public static int createCustomerQuery(String firstName, String lastName, String address, String email, String phoneNumber) throws SQLException {
+        int resultCode = -1;
+        boolean exists = checkIfEmailExists(email);
+        if (exists) {
+            System.err.println("Customer already exists with email: " + email);
+            return -1;
+        }
+        String sql = String.format("INSERT INTO customer (firstName, lastName, Address, email, phoneNumber) VALUES ('%s', '%s', '%s', '%s', '%s')",firstName, lastName, address, email, phoneNumber);
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            resultCode = pstmt.executeUpdate();
+            System.out.println("Query Successful");
+        } catch (SQLException e) {
+            System.err.println("Error with creating customer query: " + e.getMessage());
+        }
+        return resultCode;
+    }
+
+    public static boolean checkIfEmailExists(String email) throws SQLException {
+        ResultSet rs;
+        boolean exists = false;
+        String sql = "SELECT count(*) FROM customer WHERE email = ?";
+        try{
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, email);
+            rs = pstmt.executeQuery();
+            rs.next();
+            exists = rs.getInt(1) > 0;
+            System.out.println("Query Successful");
+        } catch (SQLException e) {
+            System.err.println("Error with creating customer query: " + e.getMessage());
+        }
+        return exists;
+    }
+
+
+//public static int createAccountForCustomer(String firstName, String lastName){
+//        int resultCode = -1;
+//        String sql = String.format("INSERT INTO customer (firstName, lastName, Address, email, phoneNumber) VALUES ('%s', '%s', '%s', '%s', '%s')",firstName, lastName, address, email, phoneNumber);
+//        System.out.println(sql);
+//        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+//            resultCode = pstmt.executeUpdate();
+//            System.out.println("Query Successful");
+//        } catch (SQLException e) {
+//            System.err.println("Error with creating customer query: " + e.getMessage());
+//        }
+//        return resultCode;
+//    }
 
     public static ResultSet getCustomerByAddress(String address) throws SQLException {
         Connection conn = DatabaseConnection.getConnection();
